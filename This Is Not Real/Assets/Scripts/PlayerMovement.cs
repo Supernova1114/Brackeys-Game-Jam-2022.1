0150f353ff9b79ee9e56f6c0e06daad7f5b9eccf
Supernova1114 <55326068+Supernova1114@users.noreply.github.com>
2022-02-26 13:58:19 -0800

    Added some sounds and Organized files

--- This Is Not Real/Assets/Scripts/PlayerMovement.cs
+++ This Is Not Real/Assets/Scripts/PlayerMovement.cs
@@ -12,10 +12,14 @@ public class PlayerMovement : MonoBehaviour
 
     [SerializeField]
     private float moveSpeed;
+    [SerializeField]
+    private float stepSoundInterval;
+    private float stepSoundCoolDown;
 
     private void Start()
     {
         rbody = GetComponent<Rigidbody>();
+        stepSoundCoolDown = stepSoundInterval;
     }
 
     // Update is called once per frame
@@ -24,6 +28,27 @@ public class PlayerMovement : MonoBehaviour
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
 
+        if (Mathf.Abs(horizontal) > 0 || Mathf.Abs(vertical) > 0)
+        {
+            if (stepSoundCoolDown <= 0)
+            {
+                int randomNum = Random.Range(0, 4);
+                switch (randomNum)
+                {
+                    case 0: AudioManager.instance.PlayOneShot("step1"); break;
+                    case 1: AudioManager.instance.PlayOneShot("step2"); break;
+                    case 2: AudioManager.instance.PlayOneShot("step3"); break;
+                    case 3: AudioManager.instance.PlayOneShot("step4"); break;
+                }
+
+                stepSoundCoolDown = stepSoundInterval;
+            }
+            else
+            {
+                stepSoundCoolDown -= Time.deltaTime;
+            }
+        }
+        
         
     }
 
