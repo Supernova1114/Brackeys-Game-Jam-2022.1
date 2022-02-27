36f3afd71fe1ec72dbca950f30b569927e017e3b
Supernova1114 <55326068+Supernova1114@users.noreply.github.com>
2022-02-27 00:00:10 -0800

    Added Door Teleporters and Fade to Black
    -Also set player camera height with transposer instead of offset

---
+++ This Is Not Real/Assets/Scripts/CamController.cs
@@ -0,0 +1,135 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class CamController : MonoBehaviour
+{
+
+    public static CamController instance;
+
+    [SerializeField]
+    private GameObject playerVCamObj;
+    /*[SerializeField]
+    private Collider defaultCameraBounds;*/
+    //[SerializeField]
+    //private Cinemachine.CinemachineImpulseSource impulseSource;
+
+    //private Cinemachine.CinemachineConfiner cinemachineConfiner;
+    private Cinemachine.CinemachineVirtualCamera playerVCam;
+
+
+
+    //Fade-to-black controller-----------------------------
+    [SerializeField]
+    UnityEngine.UI.Image canvasImage;
+
+    [SerializeField]
+    [Range(1, 100)]
+    private int fadeSmooth;
+
+    [SerializeField]
+    private float fadeInterval;
+
+    [SerializeField]
+    private bool fadeInOnSceneChange = true;
+
+    private bool isFading = false;
+    //-----------------------------------------------------
+
+
+    private void Start()
+    {
+        
+
+        if (fadeInOnSceneChange == true)
+        {
+            canvasImage.color = new Color(0, 0, 0, 1);
+            StartCoroutine("FadeToBlack", false);
+
+        }
+
+    }
+
+
+    void Awake()
+    {
+        instance = this;
+
+        //cinemachineConfiner = playerVCamObj.GetComponent<Cinemachine.CinemachineConfiner>();
+        //cinemachineConfiner.m_BoundingVolume = defaultCameraBounds;
+
+        playerVCam = playerVCamObj.GetComponent<Cinemachine.CinemachineVirtualCamera>();
+
+    }
+
+
+    /*public void SetBounds(Collider CameraBounds)
+    {
+        cinemachineConfiner.m_BoundingVolume = CameraBounds;
+    }*/
+
+    public void SetPosition(Vector3 position)
+    {
+        playerVCam.ForceCameraPosition(position, playerVCam.transform.rotation);
+    }
+
+    /*public void Shake()
+    {
+        impulseSource.GenerateImpulse();
+    }*/
+
+    public IEnumerator FadeToBlack(bool b)
+    {
+        if (isFading == false)
+        {
+            isFading = true;
+
+            if (b == true)
+            {
+                //fade screen to black
+                for (int i = 0; i <= 100; i += fadeSmooth)
+                {
+                    yield return new WaitForSeconds(fadeInterval);
+                    canvasImage.color = new Color(0, 0, 0, i / 100.0f);
+                }
+            }
+            else
+            {
+                //fade in screen
+                for (int i = 100; i >= 0; i -= fadeSmooth)
+                {
+                    yield return new WaitForSeconds(fadeInterval);
+                    canvasImage.color = new Color(0, 0, 0, i / 100.0f);
+                }
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Wait until fade is completed to call again!");
+        }
+
+        isFading = false;
+    }
+
+
+    public void LookAt(Transform transform)
+    {
+        playerVCam.LookAt = transform; 
+    }
+
+    public void Follow(Transform transform)
+    {
+        playerVCam.Follow = transform; 
+    }
+
+    public bool GetIsFading()
+    {
+        return isFading;
+    }
+
+
+}
+
+
---
+++ This Is Not Real/Assets/Scripts/DoorController.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoorController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform teleportDestination;
+    //[SerializeField]
+    //private Collider newCameraBounds;
+
+    [SerializeField][Range(1, 100)]
+    private int fadeSmooth;
+
+    [SerializeField]
+    private float waitBetweenFades;
+
+    private static bool isTeleporting = false;
+
+    [SerializeField]
+    private bool isLocked = false;
+    [SerializeField]
+    private int lockAmount = 0;
+
+    [SerializeField]
+    private bool isSceneChange = false;
+    [SerializeField]
+    private string nextSceneName;
+
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.CompareTag("Player") && isTeleporting == false)
+        {
+            if (isLocked == false)
+            {
+                isTeleporting = true;
+
+                StartCoroutine("BeginTeleport", collision);
+            }
+            
+        }
+    }
+
+    IEnumerator BeginTeleport(Collider collision)
+    {
+        //play door open sound
+        AudioManager.instance.Play("Door-Open");
+
+        //freeze movement
+        collision.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+
+        //fade screen to black
+        CamController.instance.StartCoroutine("FadeToBlack", true);
+        yield return new WaitUntil(() => CamController.instance.GetIsFading() == false);
+
+        if (isSceneChange == false)
+        {
+            //teleport player to specified location and update camera bounds
+            collision.transform.position = teleportDestination.position;
+            // CamController.instance.SetBounds(newCameraBounds);
+            CamController.instance.SetPosition(teleportDestination.position);
+
+            //let player fall onto floor
+            collision.GetComponent<Rigidbody>().constraints =
+                RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotation;
+
+            yield return new WaitForSeconds(waitBetweenFades);
+
+            AudioManager.instance.Play("Door-Close");
+
+            //fade in screen
+            CamController.instance.StartCoroutine("FadeToBlack", false);
+            yield return new WaitUntil(() => CamController.instance.GetIsFading() == false);
+
+            //unfreeze movement
+            collision.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+
+            isTeleporting = false;
+        }
+        else
+        {
+            SceneManager.LoadScene(sceneName: nextSceneName);
+        }
+        
+    }
+
+    public void DecreaseLock()
+    {
+        lockAmount--;
+        if (lockAmount <= 0)
+        {
+            isLocked = false;
+        }
+    }
+
+
+}
